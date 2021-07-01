8da97420e2d51a0e910700810c4868e9d1dea07c
Matt Allen Robinson <mattrobinson412@gmail.com>
2021-07-01 04:41:51 -0700

    completed divide and conquer

---
+++ count-zeroes.js
@@ -0,0 +1,21 @@
+function countZeroes() {
+    let oneZero = findFirst(arr)
+    if (oneZero === -1) return 0;
+  
+    return arr.length - oneZero
+  }
+  
+  function findFirst(arr, low = 0, high = arr.length - 1) {
+    if (high >= low) {
+      let mid = low + Math.floor((high - low) / 2)
+      if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+        return mid;
+      } else if (arr[mid] === 1) {
+        return findFirst(arr, mid + 1, high)
+      }
+      return findFirst(arr, low, mid - 1)
+    }
+    return -1;
+}
+
+module.exports = countZeroes
